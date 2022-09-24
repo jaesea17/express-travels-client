0dfed2bd72c9a667d4dcb305c048a5b52f1964fd
Oforkaja Joseph <oforkajajoseph@gmail.com>
2022-09-24 20:29:16 +0100

    changed the base url from heroku

--- src/utils/baseUrl.ts
+++ src/utils/baseUrl.ts
@@ -1,2 +1,3 @@
 // export const baseUrl = 'http://localhost:3000'
-export const baseUrl = 'https://express-travels-server.herokuapp.com'
+export const baseUrl = 'https://site--service--jr74b6rmjpn6.code.run'
+// export const baseUrl = 'https://express-travels-server.herokuapp.com'
